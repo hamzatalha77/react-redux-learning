a7752cd0acc8da54c69eb7e68f30a52f685ec598
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-21 09:38:42 +0100

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
+const store = configureStore({
+  reducer: {},
+})
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
