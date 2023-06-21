9d2c6c57c50ee08bfd10dbfaf1f71bd17a11b0e8
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-21 09:50:48 +0100

    zero

--- src/features/user.ts
+++ src/features/user.ts
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const userSlice = createSlice({
   name: 'user',
-  initialState: { value: { name: '', age: '0', email: '' } },
+  initialState: { value: { name: '', age: 0, email: '' } },
   reducers: {
     login: (state, action) => {},
   },
