ac16c42a2226f26a8bf2c9c615b1ec421c20c501
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-26 21:27:38 +0100

    send

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Profile from './components/Profile'
 import Login from './components/Login'
 import './App.css'
+import ChangeColor from './components/ChangeColor'
 const App = () => {
   return (
     <div className="App">
       <Profile />
       <Login />
+      <ChangeColor />
     </div>
   )
 }
